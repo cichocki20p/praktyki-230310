9ec697222c6f04bfcfe43d012299d06c0ed862d2
cornifer_vsc <m.cichocki.p20@gmail.com>
2023-03-14 00:02:49 +0100

    wieczorny test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000 // 8080 not working on hosting
 
 app.get('/', (req, res) => {
     res.send ('<h1>Hi hello</h1>')
+    res.send ('<h3>14.03.2023 00:01</h3>Test w domu')
 })
 
 app.listen(port, () => {
